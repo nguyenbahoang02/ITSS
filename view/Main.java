b25c97a446e8b5a6ee0760b32847a311e38bebf8
nguyenbahoang02 <nguyenbahoang02@gmail.com>
2023-06-23 00:54:18 +0700

    update 2 screen

--- view/Main.java
+++ view/Main.java
@@ -1,5 +1,27 @@
 package view;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.listOfficerView.ListOfficerViewController;
 
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/listOfficerView/ListOfficerView.fxml"));
+		loader.setController(new ListOfficerViewController(stage));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setTitle("Giao diện xem list công nhân trong đơn vị");
+		stage.setScene(scene);
+		stage.show();
+		stage.setResizable(false);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
